05ed175b19c515827976dba0ee815b1e69d3d506
swapshivam3 <72791604+swapshivam3@users.noreply.github.com>
2024-12-26 22:17:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ gr.Interface(
     title="DeepFilterNet Web UI",
     description="A simple web UI built in Gradio for audio denoising with DeepFilterNet.",
     live='true'
-).launch(inbrowser='true', share='true')
+).launch(inbrowser='true', share='true', show_error='true')
